220223d298db6ca7029335f8b44f95e20133a563
Sara Hamouda <sara.hamouda@lip6.fr>
2020-02-10 14:19:39 +0100

    using lists:foreach in place of unnecessary fold

--- src/clocksi_vnode.erl
+++ src/clocksi_vnode.erl
@@ -126,12 +126,12 @@ send_min_prepared(Partition) ->
 
 %% @doc Sends a prepare request to a Node involved in a tx identified by TxId
 prepare(ListofNodes, TxId) ->
-    lists:foldl(fun({Node, WriteSet}, _Acc) ->
+    lists:foreach(fun({Node, WriteSet}) ->
         riak_core_vnode_master:command(Node,
             {prepare, TxId, WriteSet},
             {fsm, undefined, self()},
             ?CLOCKSI_MASTER)
-    end, ok, ListofNodes).
+    end, ListofNodes).
 
 
 %% @doc Sends prepare+commit to a single partition
@@ -152,21 +152,21 @@ single_commit_sync([{Node, WriteSet}], TxId) ->
 
 %% @doc Sends a commit request to a Node involved in a tx identified by TxId
 commit(ListofNodes, TxId, CommitTime) ->
-    lists:foldl(fun({Node, WriteSet}, _Acc) ->
+    lists:foreach(fun({Node, WriteSet}) ->
         riak_core_vnode_master:command(Node,
             {commit, TxId, CommitTime, WriteSet},
             {fsm, undefined, self()},
             ?CLOCKSI_MASTER)
-    end, ok, ListofNodes).
+    end, ListofNodes).
 
 %% @doc Sends a commit request to a Node involved in a tx identified by TxId
 abort(ListofNodes, TxId) ->
-    lists:foldl(fun({Node, WriteSet}, _Acc) ->
+    lists:foreach(fun({Node, WriteSet}) ->
         riak_core_vnode_master:command(Node,
             {abort, TxId, WriteSet},
             {fsm, undefined, self()},
             ?CLOCKSI_MASTER)
-    end, ok, ListofNodes).
+    end, ListofNodes).
 
 
 %% @doc Initializes all data structures that vnode needs to track information
